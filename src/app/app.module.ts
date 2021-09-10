04b82ade548844eba62b99beb81b6dc851a621fb
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2021-09-09 22:25:46 -0300

    Router creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ROUTES } from './app.routes';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HomeComponent } from './home/home.component';
 import { GameCardComponent } from './home/game-card/game-card.component';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +24,7 @@ import { GameCardComponent } from './home/game-card/game-card.component';
     FooterComponent,
     HomeComponent,
     GameCardComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,6 +32,8 @@ import { GameCardComponent } from './home/game-card/game-card.component';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
+    MatIconModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent],
