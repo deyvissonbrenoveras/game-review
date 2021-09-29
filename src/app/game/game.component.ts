d61c4a3cda16f5a165e4f18978f16fa995551ecf
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2021-09-28 22:17:38 -0300

    Game Component Changes

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GamesService } from '../home/games.service';
+import { Game } from '../shared/game.model';
 @Component({
   selector: 'gr-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
+  @Input() game: Game;
 
-  constructor() { }
+  constructor(
+    private activatedRouted: ActivatedRoute,
+    private gamesService: GamesService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRouted.params.subscribe((params) => {
+      this.gamesService
+        .gameById(params.id)
+        .subscribe((game) => (this.game = game));
+    });
   }
-
 }
