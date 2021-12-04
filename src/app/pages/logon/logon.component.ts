0d63a57f68c152dcdd8360283867599792f94966
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2021-12-03 21:17:36 -0300

    Changes for logon component

--- src/app/pages/logon/logon.component.ts
+++ src/app/pages/logon/logon.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'gr-logon',
   templateUrl: './logon.component.html',
-  styleUrls: ['./logon.component.css']
+  styleUrls: ['./logon.component.css'],
 })
 export class LogonComponent implements OnInit {
-
-  constructor() { }
+  logonForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.logonForm = this.formBuilder.group({
+      email: this.formBuilder.control('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: this.formBuilder.control('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
   }
-
+  onSubmit(): void {
+    this.logonForm.markAllAsTouched();
+    if (this.logonForm.valid) {
+    }
+  }
 }
